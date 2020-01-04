ce781a4818f7abe2e69a8baa3e16b78ffab0cbb8
eabanonu <eabanonu@yahoo.com>
2020-01-04 16:15:13 +0100

    Query and Snapshot starter

---
+++ Akka.Persistence.Pulsar/Snapshot/PulsarSnapshotStore.cs
@@ -0,0 +1,34 @@
+﻿using Akka.Persistence.Snapshot;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akka.Persistence.Pulsar.Snapshot
+{
+    /// <summary>
+    ///     Pulsar-backed snapshot store for Akka.Persistence.
+    /// </summary>
+    public class PulsarSnapshotStore : SnapshotStore
+    {
+        protected override Task DeleteAsync(SnapshotMetadata metadata)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task DeleteAsync(string persistenceId, SnapshotSelectionCriteria criteria)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task<SelectedSnapshot> LoadAsync(string persistenceId, SnapshotSelectionCriteria criteria)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task SaveAsync(SnapshotMetadata metadata, object snapshot)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
