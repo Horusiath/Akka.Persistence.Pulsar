5db64920e95739553447912692f398a8cec03895
eabanonu <eabanonu@yahoo.com>
2020-06-04 07:37:47 +0100

    More tests

--- Akka.Persistence.Pulsar.Tests/PulsarCurrentPersistenceIdsSpec.cs
+++ Akka.Persistence.Pulsar.Tests/PulsarCurrentPersistenceIdsSpec.cs
@@ -7,10 +7,15 @@
 // -----------------------------------------------------------------------
 #endregion
 
+using System;
+using Akka.Actor;
 using Akka.Configuration;
 using Akka.Persistence.Pulsar.Query;
 using Akka.Persistence.Query;
 using Akka.Persistence.TCK.Query;
+using Akka.Streams.TestKit;
+using Akka.Util.Internal;
+using Xunit;
 using Xunit.Abstractions;
 
 namespace Akka.Persistence.Pulsar.Tests
@@ -24,6 +29,80 @@ namespace Akka.Persistence.Pulsar.Tests
         public PulsarCurrentPersistenceIdsSpec(ITestOutputHelper output) : base(SpecConfig, output: output)
         {
             ReadJournal = Sys.ReadJournalFor<PulsarReadJournal>(PulsarReadJournal.Identifier);
+        }
+
+        [Fact]
+        public override void ReadJournal_CurrentPersistenceIds_should_find_existing_events()
+        {
+            var queries = ReadJournal.AsInstanceOf<ICurrentPersistenceIdsQuery>();
+
+            Setup("a", 1);
+            Setup("b", 1);
+            Setup("c", 1);
+
+            var source = queries.CurrentPersistenceIds();
+            var probe = source.RunWith(this.SinkProbe<string>(), Materializer);
+            probe.Within(TimeSpan.FromSeconds(10), () =>
+                probe.Request(4)
+                    .ExpectNextUnordered("a", "b", "c")
+                    .ExpectComplete());
+        }
+
+        [Fact]
+        public override void ReadJournal_CurrentPersistenceIds_should_deliver_persistenceId_only_once()
+        {
+            var queries = ReadJournal.AsInstanceOf<ICurrentPersistenceIdsQuery>();
+
+            Setup("d", 100);
+
+            var source = queries.CurrentPersistenceIds();
+            var probe = source.RunWith(this.SinkProbe<string>(), Materializer);
+            probe.Within(TimeSpan.FromSeconds(10), () =>
+                probe.Request(10)
+                    .ExpectNext("d")
+                    .ExpectComplete());
+        }
+
+        [Fact]
+        public override void ReadJournal_query_CurrentPersistenceIds_should_not_see_new_events_after_complete()
+        {
+            var queries = ReadJournal.AsInstanceOf<ICurrentPersistenceIdsQuery>();
+
+            Setup("a", 1);
+            Setup("b", 1);
+            Setup("c", 1);
+
+            var greenSrc = queries.CurrentPersistenceIds();
+            var probe = greenSrc.RunWith(this.SinkProbe<string>(), Materializer);
+            probe.Request(2)
+                .ExpectNext("a")
+                .ExpectNext("c")
+                .ExpectNoMsg(TimeSpan.FromMilliseconds(100));
+
+            Setup("d", 1);
+
+            probe.ExpectNoMsg(TimeSpan.FromMilliseconds(100));
+            probe.Request(5)
+                .ExpectNext("b")
+                .ExpectComplete();
+        }
+
+        private IActorRef Setup(string persistenceId, int n)
+        {
+            var pref = Sys.ActorOf(Kits.TestActor.Props(persistenceId));
+            for (int i = 1; i <= n; i++)
+            {
+                pref.Tell($"{persistenceId}-{i}");
+                ExpectMsg($"{persistenceId}-{i}-done");
+            }
+
+            return pref;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            Materializer.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
